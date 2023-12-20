58c923268e38ad36013d91ca099c78e617cdd1c1
AdamusPL <adam.czekalski.szczecin@gmail.com>
2023-12-21 00:38:04 +0100

    Formatting fixed

--- Queries/0025_USERCONTACT_VIEW.sql
+++ Queries/0025_USERCONTACT_VIEW.sql
@@ -1,4 +1,5 @@
-SELECT Name, Surname, Email, Phone from [User] u
+CREATE VIEW UserContact AS
+SELECT Name, Surname, Email, Phone FROM [User] u
 INNER JOIN Email ON u.Id = Email.Id
 INNER JOIN Lecturer ON u.Id = Lecturer.UserId
 INNER JOIN WorkPhone ON Lecturer.UserId = WorkPhone.UserId;
--- Queries/0027_COURSEINFO_VIEW.sql
+++
@@ -1,3 +0,0 @@
-SELECT c.Name, ECTS, u.Name LecturerName, u.Surname LecturerSurname from [Course] c
-INNER JOIN Lecturer ON Lecturer.Id = c.LecturerId
-INNER JOIN [User] u ON Lecturer.UserId = u.Id;
