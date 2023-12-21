90f61a1fa722b936e5993eaa9fbdcdcea20d85f0
Maciej Padula <maciej.padula@wp.pl>
2023-12-21 21:14:14 +0100

    #71 added lecturer plan and some student fixes

--- Queries/0027_PROCEDURE_GETWEEKLYPLAN_STUDENT.sql
+++ Queries/0027_PROCEDURE_GETWEEKLYPLAN_STUDENT.sql
@@ -1,6 +1,17 @@
-CREATE OR ALTER PROCEDURE GetWeeklyPlanStudent(@StudentId INT, @StartDate DATE, @EndDate DATE)
+CREATE OR ALTER PROCEDURE GetStudentWeeklyPlan(@StudentId int, @StartDate date, @EndDate date)
 AS
 BEGIN
+    CREATE TABLE #Plan (
+        Date date,
+        Regularity varchar(255),
+        Course varchar(255),
+        GroupType varchar(255),
+        StartTime time,
+        EndTime time,
+        Classroom varchar(255),
+        Lecturer varchar(255)
+    );
+
     WHILE (@StartDate <= @EndDate)
     BEGIN
         DECLARE @DayOfWeek AS int = DATEPART(WEEKDAY, @StartDate);
@@ -11,10 +22,12 @@ BEGIN
             WHEN 1 THEN 1
         END;
         
+        INSERT INTO #Plan (Date, Regularity, Course, GroupType, StartTime, EndTime, Classroom, Lecturer)
         SELECT 
             @StartDate, 
-            Regularity.Name Regularity,
-            Course.Name, 
+            Regularity.Name AS Regularity,
+            Course.Name AS Course, 
+            GroupType.Name AS GroupType,
             StartTime, 
             EndTime, 
             Classroom, 
@@ -26,7 +39,11 @@ BEGIN
         INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
         INNER JOIN [User] u ON Lecturer.UserId = u.Id
         INNER JOIN Student_Group ON Student_Group.GroupId = g.Id
+        INNER JOIN GroupType ON g.TypeId = GroupType.Id
         WHERE Student_Group.StudentId = @StudentId AND (g.RegularityId = 0 OR g.RegularityId = @RegularityId)
         SET @StartDate = DATEADD(day, 1, @StartDate);
     END;
+
+    SELECT * FROM #Plan ORDER BY Date, StartTime;
+    DROP TABLE #Plan;
 END;
--- Queries/0028_PROCEDURE_GETWEEKLYPLAN_LECTURER.sql
+++ Queries/0028_PROCEDURE_GETWEEKLYPLAN_LECTURER.sql
@@ -1,23 +1,45 @@
-CREATE OR ALTER PROCEDURE GetWeeklyPlanLecturer(@UserId INT, @StartOfTheWeek DATE, @EndOfTheWeek DATE)
+CREATE OR ALTER PROCEDURE GetLecturerWeeklyPlan(@LecturerId int, @StartDate date, @EndDate date)
 AS
 BEGIN
+    CREATE TABLE #Plan (
+        Date date,
+        Regularity varchar(255),
+        Course varchar(255),
+        GroupType varchar(255),
+        StartTime time,
+        EndTime time,
+        Classroom varchar(255)
+    );
+
     WHILE (@StartDate <= @EndDate)
     BEGIN
+        DECLARE @DayOfWeek AS int = DATEPART(WEEKDAY, @StartDate);
+        set datefirst 1;
+        DECLARE @WeekParity AS int = DATEPART(WEEKDAY, @StartDate) % 2;
+        DECLARE @RegularityId AS int = CASE @WeekParity
+            WHEN 0 THEN 2
+            WHEN 1 THEN 1
+        END;
+        
+        INSERT INTO #Plan (Date, Regularity, Course, GroupType, StartTime, EndTime, Classroom)
         SELECT 
-            DayOfTheWeek, 
-            Regularity.Name Regularity, 
-            g.Id, 
-            FieldOfStudy.Name, 
-            Course.Name, 
+            @StartDate, 
+            Regularity.Name AS Regularity,
+            Course.Name AS Course,
+            GroupType.Name AS GroupType,
             StartTime, 
             EndTime, 
             Classroom
         FROM [Group] g
         INNER JOIN Course ON g.CourseId = Course.Id
-        INNER JOIN FieldOfStudy_Course ON Course.Id = FieldOfStudy_Course.CourseId
-        INNER JOIN FieldOfStudy ON FieldOfStudy_Course.FieldOfStudyId = FieldOfStudy.Id
         INNER JOIN Regularity ON g.RegularityId = Regularity.Id
         INNER JOIN Group_Lecturer ON g.Id = Group_Lecturer.GroupId
         INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
-        INNER JOIN [User] u ON Lecturer.UserId = u.Id
+        INNER JOIN GroupType ON g.TypeId = GroupType.Id
+        WHERE Group_Lecturer.LecturerId = @LecturerId AND (g.RegularityId = 0 OR g.RegularityId = @RegularityId)
+        SET @StartDate = DATEADD(day, 1, @StartDate);
+    END;
+
+    SELECT * FROM #Plan ORDER BY Date, StartTime;
+    DROP TABLE #Plan;
 END;
