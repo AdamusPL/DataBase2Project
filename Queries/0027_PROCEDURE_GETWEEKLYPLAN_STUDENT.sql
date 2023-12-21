7145b311d7981dc8a1da74ce66b7276c5324fc94
AdamusPL <adam.czekalski.szczecin@gmail.com>
2023-12-21 20:57:12 +0100

    Progress

--- Queries/0027_PROCEDURE_GETWEEKLYPLAN_STUDENT.sql
+++ Queries/0027_PROCEDURE_GETWEEKLYPLAN_STUDENT.sql
@@ -1,19 +1,32 @@
-CREATE OR ALTER PROCEDURE GetWeeklyPlanStudent(@UserId INT, @StartOfTheWeek DATE, @EndOfTheWeek DATE)
+CREATE OR ALTER PROCEDURE GetWeeklyPlanStudent(@StudentId INT, @StartDate DATE, @EndDate DATE)
 AS
 BEGIN
-    SELECT 
-        DayOfTheWeek, 
-        Regularity.Name Regularity,
-        Course.Name, 
-        StartTime, 
-        EndTime, 
-        Classroom, 
-        CONCAT(u.Name, ' ', u.Surname) Lecturer
-    FROM [Group] g
-    INNER JOIN Course ON g.CourseId = Course.Id
-    INNER JOIN Regularity ON g.RegularityId = Regularity.Id
-    INNER JOIN Group_Lecturer ON g.Id = Group_Lecturer.GroupId
-    INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
-    INNER JOIN [User] u ON Lecturer.UserId = u.Id
-    WHERE DayOfTheWeek >= @StartOfTheWeek AND DayOfTheWeek <= @EndOfTheWeek
+    WHILE (@StartDate <= @EndDate)
+    BEGIN
+        DECLARE @DayOfWeek AS int = DATEPART(WEEKDAY, @StartDate);
+        set datefirst 1;
+        DECLARE @WeekParity AS int = DATEPART(WEEKDAY, @StartDate) % 2;
+        DECLARE @RegularityId AS int = CASE @WeekParity
+            WHEN 0 THEN 2
+            WHEN 1 THEN 1
+        END;
+        
+        SELECT 
+            @StartDate, 
+            Regularity.Name Regularity,
+            Course.Name, 
+            StartTime, 
+            EndTime, 
+            Classroom, 
+            CONCAT(u.Name, ' ', u.Surname) Lecturer
+        FROM [Group] g
+        INNER JOIN Course ON g.CourseId = Course.Id
+        INNER JOIN Regularity ON g.RegularityId = Regularity.Id
+        INNER JOIN Group_Lecturer ON g.Id = Group_Lecturer.GroupId
+        INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
+        INNER JOIN [User] u ON Lecturer.UserId = u.Id
+        INNER JOIN Student_Group ON Student_Group.GroupId = g.Id
+        WHERE Student_Group.StudentId = @StudentId AND (g.RegularityId = 0 OR g.RegularityId = @RegularityId)
+        SET @StartDate = DATEADD(day, 1, @StartDate);
+    END;
 END;
--- Queries/0028_PROCEDURE_GETWEEKLYPLAN_LECTURER.sql
+++ Queries/0028_PROCEDURE_GETWEEKLYPLAN_LECTURER.sql
@@ -1,22 +1,23 @@
 CREATE OR ALTER PROCEDURE GetWeeklyPlanLecturer(@UserId INT, @StartOfTheWeek DATE, @EndOfTheWeek DATE)
 AS
 BEGIN
-    SELECT 
-        DayOfTheWeek, 
-        Regularity.Name Regularity, 
-        g.Id, 
-        FieldOfStudy.Name, 
-        Course.Name, 
-        StartTime, 
-        EndTime, 
-        Classroom
-    FROM [Group] g
-    INNER JOIN Course ON g.CourseId = Course.Id
-    INNER JOIN FieldOfStudy_Course ON Course.Id = FieldOfStudy_Course.CourseId
-    INNER JOIN FieldOfStudy ON FieldOfStudy_Course.FieldOfStudyId = FieldOfStudy.Id
-    INNER JOIN Regularity ON g.RegularityId = Regularity.Id
-    INNER JOIN Group_Lecturer ON g.Id = Group_Lecturer.GroupId
-    INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
-    INNER JOIN [User] u ON Lecturer.UserId = u.Id
-    WHERE DayOfTheWeek >= @StartOfTheWeek AND DayOfTheWeek <= @EndOfTheWeek
+    WHILE (@StartDate <= @EndDate)
+    BEGIN
+        SELECT 
+            DayOfTheWeek, 
+            Regularity.Name Regularity, 
+            g.Id, 
+            FieldOfStudy.Name, 
+            Course.Name, 
+            StartTime, 
+            EndTime, 
+            Classroom
+        FROM [Group] g
+        INNER JOIN Course ON g.CourseId = Course.Id
+        INNER JOIN FieldOfStudy_Course ON Course.Id = FieldOfStudy_Course.CourseId
+        INNER JOIN FieldOfStudy ON FieldOfStudy_Course.FieldOfStudyId = FieldOfStudy.Id
+        INNER JOIN Regularity ON g.RegularityId = Regularity.Id
+        INNER JOIN Group_Lecturer ON g.Id = Group_Lecturer.GroupId
+        INNER JOIN Lecturer ON Group_Lecturer.LecturerId = Lecturer.Id
+        INNER JOIN [User] u ON Lecturer.UserId = u.Id
 END;
