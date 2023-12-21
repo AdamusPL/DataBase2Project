4f607d9ba63b0cde1215074c05da0a155a1deb37
AdamusPL <adam.czekalski.szczecin@gmail.com>
2023-12-21 21:24:19 +0100

    added weighted average procedure

--- Queries/0031_PROCEDURE_AVERAGE.sql
+++ Queries/0031_PROCEDURE_AVERAGE.sql
@@ -1,10 +1,13 @@
 -- tu musi byc srednia wazona!!!
 CREATE PROCEDURE GetStudentWeightedAverageGradeInSemester(@StudentId INT, @SemesterId VARCHAR)
 AS
-SELECT Student.Id, AVG(Grade) AVERAGE
+BEGIN
+SELECT Student.Id, SUM(Grade * ECTS) / NULLIF(SUM(ECTS), 1) AS WeightedAverage
 FROM Student
 INNER JOIN Student_Group ON Student.Id = Student_Group.StudentId
 INNER JOIN Grade ON Student_Group.StudentId = Grade.StudentInGroupId
 INNER JOIN [Group] g ON Student_Group.GroupId = g.Id
-INNER JOIN Semester ON g.SemesterId = Semester.Id
-WHERE @StudentId = Student.Id AND @SemesterId = Semester.Id AND isFinal = 1
+INNER JOIN Course ON g.CourseId = Course.Id
+WHERE @StudentId = Student.Id AND @SemesterId = g.SemesterId AND isFinal = 1
+GROUP BY Student.Id
+END;
