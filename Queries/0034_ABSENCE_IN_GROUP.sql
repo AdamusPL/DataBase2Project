e85f2b230f1c53836ba22d3d7240aa5e023f7334
AdamusPL <adam.czekalski.szczecin@gmail.com>
2023-12-21 19:54:26 +0100

    BEGIN, END + fix of number absences for every student

--- Queries/0034_ABSENCE_IN_GROUP.sql
+++ Queries/0034_ABSENCE_IN_GROUP.sql
@@ -1,5 +1,6 @@
 CREATE OR ALTER PROCEDURE GetStudentAbsencesInGroup (@GroupId VARCHAR, @StudentId INT)
 AS
+BEGIN
 SELECT Student.Id, u.Name, u.Surname, Absence.Date AbsenceDate
 FROM Student
 INNER JOIN Student_Group ON Student.Id = Student_Group.StudentId
@@ -7,3 +8,4 @@ INNER JOIN [Group] g ON Student_Group.GroupId = g.Id
 INNER JOIN [User] u ON Student.UserId = u.Id
 INNER JOIN Absence ON Student_Group.StudentId = Absence.Id
 WHERE @GroupId = g.Id AND @StudentId = Student.Id;
+END;
