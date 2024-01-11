471e2317679d073bcafe73e50e84799db5ee4207
Maciej Padula <maciej.padula@wp.pl>
2024-01-11 21:05:59 +0100

    Update 0019_CREATE_GRADE.sql

--- Queries/0019_CREATE_GRADE.sql
+++ Queries/0019_CREATE_GRADE.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Grade (
     Id int IDENTITY(1, 1) NOT NULL,
     Text nvarchar(50) NOT NULL,
-    Grade int NOT NULL,
+    Grade decimal(2, 1) NOT NULL,
     Accepted bit NULL,
     IsFinal bit NOT NULL,
     StudentInGroupId int NOT NULL FOREIGN KEY REFERENCES Student_Group(Id) ON DELETE CASCADE,
