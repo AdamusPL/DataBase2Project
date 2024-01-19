21d6feb3891fce033b3da46c863c645247e2c421
MaciejPadula <maciej.padula@wp.pl>
2024-01-19 10:12:10 +0100

    fix txt

--- Application/Jsos3/Jsos3.Authorization/ViewModels/LoginViewModel.cs
+++ Application/Jsos3/Jsos3.Authorization/ViewModels/LoginViewModel.cs
@@ -4,7 +4,7 @@ namespace Jsos3.Authorization.ViewModels
 {
     public class LoginViewModel
     {
-        [Display(Name = "Nazwa używtkonika")]
+        [Display(Name = "Nazwa użytkownika")]
         public string Login { get; set; }
         [Display(Name = "Hasło")]
         public string Password { get; set; }
